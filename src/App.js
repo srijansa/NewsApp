c33c5d91ab475bffd76997f354af7a6b2ef911a7
srijansa <159839387+srijansa@users.noreply.github.com>
2025-02-11 02:10:52 -0500

    Changing to fucntional components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+
 export default function App() {
   return (
     <div>
       <NavBar/>
       <div className="container">
         <News/>
-        
       </div>
     </div>
   )
