4171d3d959a3cfc803faba54d131b2932c8b869a
srijansa <159839387+srijansa@users.noreply.github.com>
2025-02-11 02:59:46 -0500

    Changing NavBar and News

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, {useState, useEffect, useMemo} from "react";
+import NewsItem from "./NewsItem";
 
-export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is a news component.
-        <NewsItem/>
-      </div>
-    )
-  }
+const News = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = userState("");
+  const [theme, setTheme] = useState("light");
+
+  useEffect(()=>{
+    async function fetchNews(){
+      const response = await fetch("https://api.coingecko.com/api/v3/news");
+      const data = await response.json();
+      setArticles(data.data);
+      setLoading(false);
+    }
+    fetchNews();
+}, []);
+  
+  const handleScroll = useCallback( ()=> {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
+      setPage(prevPage => prevPage + 1);
+    }
+    }, []
+  );
+
 }
 
-export default News
+export default News;
